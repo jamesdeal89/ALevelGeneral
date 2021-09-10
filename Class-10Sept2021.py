cc549ebe7d1018832f753b0d88ae54711be98db0
Jamie <j891319@protonmail.com>
2021-09-10 15:11:37 +0800

    added file loops and Q bank

--- Class-10Sept2021.py
+++ Class-10Sept2021.py
@@ -1,11 +1,31 @@
+import random
+
 name = input("what is your name?:")
 print("hello" + name)
 
 while True:
-    print("ask me anything")
-    question = input(":")
-    if "capital" in question.lower():
-        if "france" in question.lower():
+    print("ask me anything!")
+    question = input(":").lower()
+    if "capital" in question:
+        if "france" in question:
             print("F")
-        if "spain" in question.lower():
+        elif "spain" in question:
             print("S")
+        else:
+            print("I don't know that capital.")
+    elif "cia" in question:
+        print("I'm not at liberty to say...")
+        print("How about I ask you some questions now?")
+        break
+    else:
+        print("I don't understand.")
+
+questionBank = ["how are you?:", 
+                "are you wealthy?:",
+                "what is your location?:"
+                "what is your occupation?:"
+                "Am I your friend?:"
+                "Am I your soulmate?:"
+                "What did you eat earlier?"]
+while True:
+    ans = input(random.choice(questionBank))
