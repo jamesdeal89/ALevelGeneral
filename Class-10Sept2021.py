8f97e8174e9097e6550ba5102204fcb01a3b190e
Jamie <j891319@protonmail.com>
2021-09-10 14:47:34 +0800

    added basic chatbot loop

--- Class-10Sept2021.py
+++ Class-10Sept2021.py
@@ -1,2 +1,11 @@
 name = input("what is your name?:")
-print("hello", name)
+print("hello" + name)
+
+while True:
+    print("ask me anything")
+    question = input(":")
+    if "capital" in question.lower():
+        if "france" in question.lower():
+            print("F")
+        if "spain" in question.lower():
+            print("S")
