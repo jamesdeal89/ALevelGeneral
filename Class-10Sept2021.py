cee8da913dd658d64f31c1a35a407edcfb50091a
Jamie <j891319@protonmail.com>
2021-09-13 09:00:49 +0800

    greetings + names random lists, initialised class

--- Class-10Sept2021.py
+++ Class-10Sept2021.py
@@ -1,10 +1,30 @@
 import random
 
 # chatbot, class which contains some functions and holds information in attributes
+class Chatbot():
     # initialize our class object
+    # here we pass parameters into this function:
+    # 'self', the class we're in, and 'name', with a default value
+    def __init__(self, name = ""):
+        # the '.' shows that 'personality' is a part of the 'Chatbot' class
+        # self refers to the class code is in, without having to write 'Chatbot.personality'
+        self.personality = random.choice(["aggressive","dismissive","suprised","chipper"])
+        if name == "":
+            self.name = random.choice(["James","Mary","Robert","Patricia","John","Jennifer","Michael","Linda","William","Elizabeth","David","Barbara","Richard","Susan","Joseph","Jessica","Thomas","Sarah","Charles","Karen","Christopher","Nancy","Daniel","Lisa","Matthew","Betty","Anthony","Margaret","Mark","Sandra","Donald","Ashley","Steven","Kimberly","Paul","Emily","Andrew","Donna","Joshua","Michelle","Kenneth","Dorothy","Kevin","Carol","Brian","Amanda","George","Melissa","Edward","Deborah","Ronald","Stephanie","Timothy","Rebecca","Jason","Sharon","Jeffrey","Laura","Ryan","Cynthia","Jacob","Kathleen","Gary","Amy","Nicholas","Shirley","Eric","Angela","Jonathan","Helen","Stephen","Anna","Larry","Brenda","Justin","Pamela","Scott","Nicole","Brandon","Emma","Benjamin","Samantha","Samuel","Katherine","Gregory","Christine","Frank","Debra","Alexander","Rachel","Raymond","Catherine","Patrick","Carolyn","Jack","Janet","Dennis","Ruth","Jerry","Maria","Tyler","Heather","Aaron","Diane","Jose","Virginia","Adam","Julie","Henry","Joyce","Nathan","Victoria","Douglas","Olivia","Zachary","Kelly","Peter","Christina","Kyle","Lauren","Walter","Joan","Ethan","Evelyn","Jeremy","Judith","Harold","Megan","Keith","Cheryl","Christian","Andrea","Roger","Hannah","Noah","Martha","Gerald","Jacqueline","Carl","Frances","Terry","Gloria","Sean","Ann","Austin","Teresa","Arthur","Kathryn","Lawrence","Sara","Jesse","Janice","Dylan","Jean","Bryan","Alice","Joe","Madison","Jordan","Doris","Billy","Abigail","Bruce","Julia","Albert","Judy","Willie","Grace","Gabriel","Denise","Logan","Amber","Alan","Marilyn","Juan","Beverly","Wayne","Danielle","Roy","Theresa","Ralph","Sophia","Randy","Marie","Eugene","Diana","Vincent","Brittany","Russell","Natalie","Elijah","Isabella","Louis","Charlotte","Bobby","Rose","Philip","Alexis","Johnny","Kayla"])
+        else:
+            # here 'self.name' instead of just 'name' means we ensure we're referencing the 'name' of the class
+            # ,not just any other name in the program
+            self.name = name
+        self.greetings = ["hello","hi","howdy","aloha","hi-ya","hey","wassup","hi","good evening","what's up?","how's it going?","welcome","buenos dias","hiya","sup","yo","bonjour","good morrow","shalom"]
     # main function
-    # 
+    def chat(self):
+        print(random.choice(self.greetings))
+    # function to ask question
+    # function to find out information
 
+# example notes 
+bob = Chatbot # creating an instance of the class 'Chatbot'
+alice = Chatbot # creating a second instance of the class 'Chatbot'
 
 
 name = input("what is your name?:")
